ede4072787b498391441238d0cfcb00f6de13eec
TuanTA <tuanta@vistarsoft.com>
2023-08-17 17:23:06 +0700

    update design login and signup

--- frontend/src/page/login/Login.jsx
+++ frontend/src/page/login/Login.jsx
@@ -1,4 +1,4 @@
-import { React, useRef } from 'react';
+import { React, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Login.scss';
 
@@ -6,8 +6,37 @@ import './Login.scss';
 import LogoReact from '../../assest/react.svg';
 
 const Login = () => {
-  const emailRef = useRef();
-  const passwordRef = useRef();
+  const [data, setData] = useState({
+    email : "",
+    password : "",
+  })
+
+  const handleOnChange = (e) => {
+    const {name, value} = e.target;
+
+    setData((preve) => {
+      return {
+        ...preve,
+        [name] : value
+      }
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const {email, password} = data;
+
+    if ( email && password) {
+      if (password) {
+        alert("success.");
+      } else {
+        alert("Mật khẩu và xác nhận mật khẩu không giống nhau.");
+      }
+    } else {
+      alert("Vui lòng nhập các trường bắt buộc.")
+    }
+  }
 
   return (
     <div className='login'>
@@ -18,10 +47,26 @@ const Login = () => {
           <h1 className='text-center p-4'>Đăng Nhập</h1>
 
           <div className='form-login w-full max-w-md p-5 md:p-5 text-center bg-white m-auto'>
-              <form action="">
-                <input ref={emailRef} type="email" id='emailName' className='form-control w-full' placeholder="Email" aria-label='Email Address'/>
-                <input ref={passwordRef} type="password" id='password' className='form-control w-full' placeholder="Mật khẩu" aria-label='Password'/>
-                <button className="btn btn-primary w-full mb-4">Đăng nhập</button>
+              <form onSubmit={handleSubmit} action="">
+                <input  type="email" 
+                        name='email'
+                        value={data.email}
+                        onChange={handleOnChange}
+                        className='form-control w-full' 
+                        placeholder="Email" 
+                        aria-label='Email Address'
+                        required
+                />
+                <input  type="password" 
+                        name='password'
+                        value={data.password}
+                        onChange={handleOnChange}
+                        className='form-control w-full' 
+                        placeholder="Mật khẩu" 
+                        aria-label='Password'
+                        required
+                />
+                <button type='submit' className="btn btn-primary w-full mb-4">Đăng nhập</button>
                 <p className="message">Chưa có tài khoản? <Link to="/signup">Đăng ký</Link></p>
               </form>
             </div>
--- frontend/src/page/signup/Signup.jsx
+++ frontend/src/page/signup/Signup.jsx
@@ -1,4 +1,4 @@
-import { React, useRef } from 'react';
+import { React, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Signup.scss';
 
@@ -7,10 +7,39 @@ import LogoReact from '../../assest/react.svg';
 
 
 const Signup = () => {
-  const nameRef = useRef();
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const passwordConfirmationRef = useRef();
+  const [data, setData] = useState({
+    fullName : "",
+    email : "",
+    password : "",
+    passwordConfirmation : "",
+  })
+
+  const handleOnChange = (e) => {
+    const {name, value} = e.target;
+
+    setData((preve) => {
+      return {
+        ...preve,
+        [name] : value
+      }
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const {fullName, email, password, passwordConfirmation} = data;
+
+    if (fullName && email && password && passwordConfirmation) {
+      if (password === passwordConfirmation) {
+        alert("success.");
+      } else {
+        alert("Mật khẩu và xác nhận mật khẩu không giống nhau.");
+      }
+    } else {
+      alert("Vui lòng nhập các trường bắt buộc.")
+    }
+  }
 
   return (
     <div className='signup'>
@@ -21,12 +50,45 @@ const Signup = () => {
           <h1 className='text-center p-4'>Đăng Ký</h1>
 
           <div className='form-signup w-full max-w-md p-5 md:p-5 text-center bg-white m-auto'>
-              <form action="">
-                <input ref={nameRef} type="text" id='firstName' className='form-control w-full' placeholder="Họ và tên" aria-label='Full Name'/>
-                <input ref={emailRef} type="email" id='emailName' className='form-control w-full' placeholder="Email" aria-label='Email Address'/>
-                <input ref={passwordRef} type="password" id='password' className='form-control w-full' placeholder="Mật khẩu" aria-label='Password'/>
-                <input ref={passwordConfirmationRef} type="password" id='password' className='form-control w-full' placeholder="Lặp lại mật khẩu" aria-label='Repeat Password'/>
-                <button className="btn btn-primary w-full mb-4">Đăng ký</button>
+              <form onSubmit={handleSubmit} action="">
+                <input  type="text" 
+                        name='fullName'
+                        value={data.fullName}
+                        onChange={handleOnChange}
+                        className='form-control w-full' 
+                        placeholder="Họ và tên" 
+                        aria-label='Full Name'
+                        required
+                />
+                <input  type="email" 
+                        name='email'
+                        value={data.email}
+                        onChange={handleOnChange}
+                        className='form-control w-full' 
+                        placeholder="Email" 
+                        aria-label='Email Address'
+                        required
+                />
+                <input  type="password" 
+                        name='password'
+                        value={data.password}
+                        onChange={handleOnChange}
+                        className='form-control w-full' 
+                        placeholder="Mật khẩu" 
+                        aria-label='Password'
+                        required
+                />
+                <input  type="password" 
+                        name='passwordConfirmation'
+                        value={data.passwordConfirmation}
+                        onChange={handleOnChange}
+                        className='form-control w-full' 
+                        placeholder="Lặp lại mật khẩu" 
+                        aria-label='Repeat Password'
+                        required
+                  />
+
+                <button type='submit' className="btn btn-primary w-full mb-4">Đăng ký</button>
                 <p className="message">Đã đăng ký? <Link to="/login">Đăng nhập</Link></p>
               </form>
             </div>
